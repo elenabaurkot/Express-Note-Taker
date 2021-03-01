6ecae85ac3a4f2021272193186ef47f4152fa002
Zhylow <pk1blue@yahoo.com>
2021-02-28 21:12:29 -0500

    added css usage to server

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ const app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 const fs = require("fs");
 
 require("./public/routes/apiRoutes")(app);
